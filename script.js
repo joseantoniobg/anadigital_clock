8ab55870a1bf568ac685a449aa801afe82f605fc
José Antônio <joseantonio1@hotmail.com.br>
2024-01-21 17:33:57 -0300

    feat: removed interval const, added different lengths to minute and hour

--- script.js
+++ script.js
@@ -9,23 +9,24 @@ function addZero(i) {
   return i;
 }
 
-function getClockCell(number, top, deg, className) {
+function getClockCell(number, top, deg, className, invisible) {
   const element = document.createElement('div');
   element.className = `cell ${className}`;
   element.style.top = `${top}px`;
   element.innerHTML = number;
   element.style.transform = `rotate(-${deg}deg)`;
+  if (invisible) element.style.display = 'none';
   return element;
 }
 
-function drawClockPoint(deg, value, className, space, left, width) {
+function drawClockPoint(countPoints, deg, value, className, space, left, width) {
   const clockPointDiv = document.createElement('div');
   clockPointDiv.className = 'clock-point';
   clockPointDiv.style.width = `${width}px`;
   clockPointDiv.style.left = `${left}px`;
   clockPointDiv.style.transform = `rotate(${value * deg}deg)`;
   for (let i = 0; i < 7; i++) {
-    clockPointDiv.appendChild(getClockCell(addZero(value), i * space, value * deg, className));
+    clockPointDiv.appendChild(getClockCell(addZero(value), i * space, value * deg, className, countPoints > i));
   }
   clock.appendChild(clockPointDiv);
 }
@@ -33,12 +34,12 @@ function drawClockPoint(deg, value, className, space, left, width) {
 function draw() {
   const d = new Date();
   clock.innerHTML = '';
-  drawClockPoint(degHours, d.getHours(), 'hour-cell', 48.5, 290, 40);
-  drawClockPoint(degMinutes, d.getMinutes(), 'minute-cell', 42, 295, 30);
-  drawClockPoint(degSecs, d.getSeconds(), 'second-cell', 43, 298, 24);
+  drawClockPoint(2, degHours, d.getHours(), 'hour-cell', 48.5, 290, 40);
+  drawClockPoint(1, degMinutes, d.getMinutes(), 'minute-cell', 42, 295, 30);
+  drawClockPoint(0, degSecs, d.getSeconds(), 'second-cell', 43, 298, 24);
 }
 
-const interval = setInterval(() => {
+setInterval(() => {
   draw();
 }, 1000);
 
