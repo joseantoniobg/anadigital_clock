3a170e6cae203794a95b9f820c9eb079bfde4855
José <joseantonio1@hotmail.com.br>
2024-01-22 14:39:36 -0300

    feat: add animation to all clock pointers

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const degSecs = 360 / 60;
 const degMinutes = degSecs;
 const degHours = 360 / 12;
+const hourInterval = 1 / 3600;
+const minuteInterval = 1 / 60;
 
 const clock = document.getElementById("clock");
 
@@ -19,24 +21,40 @@ function getClockCell(number, top, deg, className, invisible) {
   return element;
 }
 
-function drawClockPoint(countPoints, deg, value, className, space, left, width) {
+function drawClockPoint(countPoints, deg, value, className, space, left, width, begin, interval) {
   const clockPointDiv = document.createElement('div');
   clockPointDiv.className = 'clock-point';
   clockPointDiv.style.width = `${width}px`;
   clockPointDiv.style.left = `${left}px`;
-  clockPointDiv.style.transform = `rotate(${value * deg}deg)`;
+
+  const newspaperSpinning = [
+    { transform: `rotate(${(value + begin) * deg}deg)` },
+    { transform: `rotate(${(value + begin + interval) * deg}deg)` },
+  ];
+
+  clockPointDiv.animate(newspaperSpinning, { duration: 1000 });
+
   for (let i = 0; i < 7; i++) {
-    clockPointDiv.appendChild(getClockCell(addZero(value), i * space, value * deg, className, countPoints > i));
+    clockPointDiv.appendChild(getClockCell(addZero(value), i * space, (value + begin + interval) * deg, className, countPoints > i));
   }
   clock.appendChild(clockPointDiv);
 }
 
+let lastMinute = -1;
+let lastHour = -1;
+
 function draw() {
   const d = new Date();
   clock.innerHTML = '';
-  drawClockPoint(2, degHours, d.getHours(), 'hour-cell', 48.5, 290, 40);
-  drawClockPoint(1, degMinutes, d.getMinutes(), 'minute-cell', 42, 295, 30);
-  drawClockPoint(0, degSecs, d.getSeconds(), 'second-cell', 43, 298, 24);
+
+  lastMinute = lastMinute === -1 ? d.getMinutes() : lastMinute;
+  lastHour = lastHour === -1 ? d.getHours() : lastHour;
+
+  drawClockPoint(2, degHours, d.getHours(), 'hour-cell', 48.5, 290, 40, (d.getMinutes() * 60 + d.getSeconds()) / 3600, hourInterval);
+  drawClockPoint(1, degMinutes, d.getMinutes(), 'minute-cell', 42, 295, 30, d.getSeconds() / 60, minuteInterval);
+  drawClockPoint(0, degSecs, d.getSeconds(), 'second-cell', 43, 300, 20, 0, 1);
+  lastMinute = d.getMinutes();
+  lastHour = d.getHours();
 }
 
 setInterval(() => {
